9ebdc7023810b921a128f430e86f4faff48e9ef5
박찬의 <pon03113@naver.com>
2022-01-26 16:41:57 +0900

    feat:[front]Petpage table css 적용중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {Signup, Login} from './components/Mypage'
 import Mypage from './components/Mypage/Mypage'
+import Petpage from './components/Petpage/Petpage'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         기본
-        <Mypage id="sw"/>
+        <Petpage/>
         <Routes>
           <Route path="/signup" element={<Signup />}/>
           <Route path="/login" element={<Login />}/>
         </Routes>
       </div>
     </BrowserRouter>
-
+    
   );
 }
 
