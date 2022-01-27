1d0ea76717012dd8e2abc40e8b11b7c30cebc0d5
안재현 <sevct@naver.com>
2022-01-27 13:32:59 +0900

    feat:[back]컨트롤러 매핑 변경

--- backend-java/src/main/java/com/ssafy/api/controller/AuthController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.ssafy.api.controller;
 
+import com.ssafy.api.request.UserRegisterPostReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,7 +28,7 @@ import io.swagger.annotations.ApiResponse;
  */
 @Api(value = "인증 API", tags = {"Auth."})
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/restAuth")
 public class AuthController {
 	@Autowired
 	UserService userService;
@@ -55,5 +56,25 @@ public class AuthController {
 		}
 		// 유효하지 않는 패스워드인 경우, 로그인 실패로 응답.
 		return ResponseEntity.status(401).body(UserLoginPostRes.of(401, "Invalid Password", null));
+	}
+
+	@PostMapping("/signup")
+	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.")
+	@ApiResponses({
+			@ApiResponse(code = 200, message = "성공"),
+			@ApiResponse(code = 401, message = "인증 실패"),
+			@ApiResponse(code = 404, message = "사용자 없음"),
+			@ApiResponse(code = 500, message = "서버 오류")
+	})
+	public ResponseEntity<? extends BaseResponseBody> register(
+			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
+		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
+
+		if(userService.checkUser(registerInfo.getUserEmail())){
+			return ResponseEntity.status(404).body(BaseResponseBody.of(404, "Fail"));
+		}else{
+			User user = userService.createUser(registerInfo);
+		}
+		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
 	}
 }
--- backend-java/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/UserController.java
@@ -31,7 +31,7 @@ import springfox.documentation.annotations.ApiIgnore;
  */
 @Api(value = "유저 API", tags = {"User"})
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/myPage")
 public class UserController {
 	
 	@Autowired
@@ -40,27 +40,27 @@ public class UserController {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 
-	@PostMapping()
-	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.") 
-    @ApiResponses({
-        @ApiResponse(code = 200, message = "성공"),
-        @ApiResponse(code = 401, message = "인증 실패"),
-        @ApiResponse(code = 404, message = "사용자 없음"),
-        @ApiResponse(code = 500, message = "서버 오류")
-    })
-	public ResponseEntity<? extends BaseResponseBody> register(
-		@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
-		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
-
-		if(userService.checkUser(registerInfo.getUserEmail())){
-			return ResponseEntity.status(404).body(BaseResponseBody.of(404, "Fail"));
-		}else{
-			User user = userService.createUser(registerInfo);
-		}
-		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
-	}
+//	@PostMapping("signup")
+//	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.")
+//    @ApiResponses({
+//        @ApiResponse(code = 200, message = "성공"),
+//        @ApiResponse(code = 401, message = "인증 실패"),
+//        @ApiResponse(code = 404, message = "사용자 없음"),
+//        @ApiResponse(code = 500, message = "서버 오류")
+//    })
+//	public ResponseEntity<? extends BaseResponseBody> register(
+//		@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
+//		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
+//
+//		if(userService.checkUser(registerInfo.getUserEmail())){
+//			return ResponseEntity.status(404).body(BaseResponseBody.of(404, "Fail"));
+//		}else{
+//			User user = userService.createUser(registerInfo);
+//		}
+//		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+//	}
 	
-	@GetMapping("/myPage")
+	@GetMapping()
 	@ApiOperation(value = "회원 본인 정보 조회", notes = "로그인한 회원 본인의 정보를 응답한다.") 
     @ApiResponses({
         @ApiResponse(code = 200, message = "성공"),
@@ -97,7 +97,7 @@ public class UserController {
 	}
 
 	// Update(갱신)
-	@PatchMapping("/myPage")
+	@PatchMapping()
 	@ApiOperation(value = "userId 회원 수정", notes = "해당 아이디 회원의 정보를 수정한다.")
 	@ApiResponses({
 			@ApiResponse(code = 200, message = "성공"),
@@ -120,7 +120,7 @@ public class UserController {
 	}
 
 	// 삭제
-	@DeleteMapping("/myPage")
+	@DeleteMapping()
 	@ApiOperation(value = "유저 정보 삭제", notes = "유저 정보 삭제")
 	@ApiResponses({
 			@ApiResponse(code = 200, message = "성공"),
