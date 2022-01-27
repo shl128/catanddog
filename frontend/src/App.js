d85c8e2a29acb761dee27cfc45cc3d5df32b306e
안재현 <sevct@naver.com>
2022-01-27 10:44:04 +0900

    회원가입CRUD

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {Signup, Login} from './components/Mypage'
-import Mypage from './components/Mypage/Mypage'
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        기본
-        <Mypage id="sw"/>
-        <Routes>
-          <Route path="/signup" value="회원가입" element={<Signup />}/>
-          <Route path="/login" value="로그인" element={<Login />}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
-
+    <div>
+      <Link to="/mypage">앱</Link>
+    </div>
   );
 }
 
