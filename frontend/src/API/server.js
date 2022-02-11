b5be59993af3bf51f5b7bf40f1193434efc7a03e
sangwoo <hanomon@naver.com>
2022-02-11 10:57:02 +0900

    feat:[front]마이페이지 이미지 수정 완료

--- frontend/src/API/server.js
+++ frontend/src/API/server.js
@@ -18,6 +18,7 @@ export default {
     // createPetInformation(Petpage)
     createPet: 'petPage/pets/',
     // spendingOfMonth
-    Expenditure: 'spendingOfMonth/expenditures'
+    Expenditure: 'spendingOfMonth/expenditures',
+    userPhoto: 'api/v1/myPage/user_photo'
   },
 };
