8da8414d49893a1c144be273a898f1b9526bd045
shl128 <shl128@naver.com>
2022-02-23 12:18:56 +0900

    feat:[front+back]배포설정으로 변경

--- frontend/src/API/server.js
+++ frontend/src/API/server.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 export default {
 
-  BASE_URL: 'http://localhost:8086/api/',
-  //BASE_URL: 'https://i6b109.p.ssafy.io:8086/api/',  // 배포
-  REDIRECT_URI: 'http://localhost:3000/',
-  //REDIRECT_URI: 'http://i6b109.p.ssafy.io/',  // 배포
+  //BASE_URL: 'http://localhost:8086/api/',
+  BASE_URL: 'https://i6b109.p.ssafy.io:8086/api/',  // 배포
+  //REDIRECT_URI: 'http://localhost:3000/',
+  REDIRECT_URI: 'http://i6b109.p.ssafy.io/',  // 배포
 
   ROUTES: {
     // Account
